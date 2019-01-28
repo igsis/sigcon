b71e95c95ed8310a812c29fb82421b6b47f724ba
Diego Vicente <diego.vdossantos@gmail.com>
2019-01-28 18:05:02 -0200

    Cadastro / Listagem Equipamento

--- perfil/administrativo/includes/menu.php
+++ perfil/administrativo/includes/menu.php
@@ -6,22 +6,22 @@
         <ul class="sidebar-menu" data-widget="tree">
             <li><a href="?secao=perfil"><i class="fa fa-home"></i><span>Home</span></a></li>
             <li class="header">MÓDULOS</li>
-            <li class="treeview">
-                <a href=""> <i class="fa fa-cogs"></i> <span>Cadastrar</span> <i class="fa fa-angle-left pull-right"></i> </a>
-                <ul class="treeview-menu">
-                    <li><a href="?perfil=administrativo&p=licitacao&sp=licitacao_cadastro"><i class="fa fa-circle-o text-red"></i> <span>Licitação</span></a></li>
-                    <li><a href="?perfil=administrativo&p=unidades&sp=unidade_cadastro"><i class="fa fa-circle-o text-red"></i> <span>Unidades</span></a></li>
-                    <li><a href="?perfil=administrativo&p=equipamentos&sp=equipamento_cadastro"><i class="fa fa-circle-o text-red"></i> <span>Equipamentos</span></a></li>
-                    <li><a href="?perfil=administrativo&p=usuario&sp=usuario_cadastro"><i class="fa fa-circle-o text-red"></i> <span>Usuários</span></a></li>
-                </ul>
-            </li>
+            <li> <a href="?perfil=administrativo&p=licitacao&sp=licitacao_cadastro"> <i class="fa fa-circle-o text-orange"></i> <span>Cadastrar Licitação</span> </a> </li>
+            <li> <a href="?perfil=administrativo&p=usuario&sp=usuario_cadastro"> <i class="fa fa-circle-o text-green"></i> <span>Cadastrar Usuário</span> </a> </li>
             <li class="treeview">
                 <a href=""> <i class="fa fa-search"></i> <span>Pesquisar</span> <i class="fa fa-angle-left pull-right"></i> </a>
                 <ul class="treeview-menu">
                     <li><a href="?perfil=administrativo&p=pesquisa&sp=pesquisa_licitacao"><i class="fa fa-circle-o text-red"></i> <span>Licitação</span></a></li>
-                    <li><a href="?perfil=administrativo&p=pesquisa&sp=pesquisa_unidade"><i class="fa fa-circle-o text-red"></i> <span>Unidade</span></a></li>
                     <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Contratos</span></a></li>
                     <li><a href="?perfil=administrativo&p=pesquisa&sp=pesquisa_usuario"><i class="fa fa-circle-o text-red"></i> <span>Usuários</span></a></li>
+                </ul>
+            </li>
+            <li class="treeview">
+                <a href=""> <i class="fa fa-cogs"></i> <span>Gerenciamento do Sistema</span> <i class="fa fa-angle-left pull-right"></i> </a>
+                <ul class="treeview-menu">
+                    <li><a href="?perfil=administrativo&p=unidades&sp=unidade_cadastro"><i class="fa fa-circle-o text-red"></i> <span>Unidades</span></a></li>
+                    <li><a href="?perfil=administrativo&p=pesquisa&sp=pesquisa_equipamento"><i class="fa fa-circle-o text-red"></i> <span>Equipamentos</span></a></li>
+                    <li><a href="?perfil=administrativo&p=usuario&sp=usuario_cadastro"><i class="fa fa-circle-o text-red"></i> <span>Usuários</span></a></li>
                 </ul>
             </li>
             <li class="header">MAIS</li>
