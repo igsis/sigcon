00d68d8300e83bcbbcd7a2a80d17f3e333ff855f
Isaías Mendes <isaiaszmendes@gmail.com>
2019-01-31 17:23:37 -0200

    cadastro edita checkbox concluido

--- perfil/administrativo/licitacao/licitacao_edita.php
+++ perfil/administrativo/licitacao/licitacao_edita.php
@@ -164,28 +164,28 @@ $status = recuperaDados("licitacao_status","id",$licitacao['licitacao_status_id'
                                 <div class="form-group col-md-3">
                                     <div class="checkbox">
                                         <label for="levantamento_preco" class='text-center'><strong>Levantamento de preço?</strong>                                
-                                            <input type="checkbox" name='levantamento_preco' id='levantamento_preco'  <?= $licitacao['levantamento_preco'] ? 'checked' : NULL ?>/>
+                                            <input type="checkbox" class="check" name='levantamento_preco' id='levantamento_preco'  <?= $licitacao['levantamento_preco'] ? 'checked' : NULL ?>/>
                                         </label>                                        
                                     </div>
                                 </div>
                                 <div class="form-group col-md-3">
                                     <div class="checkbox">
                                         <label for="reserva" class='text-center'><strong>Reserva? </strong>
-                                            <input type="checkbox" name="reserva" id='reserva'  <?= $licitacao['reserva'] ? 'checked' : NULL ?> />
+                                            <input type="checkbox" class="check" name="reserva" id='reserva'  <?= $licitacao['reserva'] ? 'checked' : NULL ?> />
                                         </label>                                      
                                     </div>                                
                                 </div>
                                 <div class="form-group col-md-3">
                                     <div class="checkbox">
                                         <label for="elaboracao_edital" class='text-center'><strong>Elaboração de Edital?</strong>
-                                            <input type="checkbox" name="elaboracao_edital" id="elaboracao_edital" <?= $licitacao['elaboracao_edital'] ? 'checked' : NULL ?> />
+                                            <input type="checkbox" class="check" name="elaboracao_edital" id="elaboracao_edital" <?= $licitacao['elaboracao_edital'] ? 'checked' : NULL ?> />
                                         </label> 
                                     </div>                                    
                                 </div>
                                 <div class="form-group col-md-3">
                                     <div class="checkbox">
                                         <label for="analise_edital" class='text-center'><strong>Análise / Ajuste do Edital? </strong>
-                                            <input type="checkbox" name="analise_edital"  id='analise_edital' <?= $licitacao['analise_edital'] ? 'checked' : NULL ?> /> 
+                                            <input type="checkbox" class="check" name="analise_edital"  id='analise_edital' <?= $licitacao['analise_edital'] ? 'checked' : NULL ?> /> 
                                         </label>
                                     </div>                                
                                 </div>
@@ -203,7 +203,7 @@ $status = recuperaDados("licitacao_status","id",$licitacao['licitacao_status_id'
                                 <div class="form-group col-md-3">
                                     <div class="checkbox">
                                         <label for="homologacao" class='text-center'><strong>Homologação / Recurso?</strong>
-                                            <input type="checkbox" name="homologacao" id='homologacao' <?= $licitacao['homologacao'] ? 'checked' : NULL ?> />
+                                            <input type="checkbox" class="check" name="homologacao" id='homologacao' <?= $licitacao['homologacao'] ? 'checked' : NULL ?> />
                                         </label>
                                     </div>                                   
                                 </div>
@@ -218,7 +218,7 @@ $status = recuperaDados("licitacao_status","id",$licitacao['licitacao_status_id'
                                 <div class="form-group col-md-2">
                                     <div class="checkbox">
                                         <label for="empenho" class='text-center'><strong>Empenho?</strong>
-                                            <input type="checkbox" name="empenho" id='empenho' <?= $licitacao['empenho'] ? 'checked' : NULL ?> />
+                                            <input type="checkbox" class="check" name="empenho" id='empenho' <?= $licitacao['empenho'] ? 'checked' : NULL ?> />
                                         </label>
                                     </div>                                            
                                 </div>
@@ -229,7 +229,7 @@ $status = recuperaDados("licitacao_status","id",$licitacao['licitacao_status_id'
                                 <div class="form-group col-md-2">
                                     <div class="checkbox">
                                         <label for="entrega" class='text-center'> <strong>Entrega? </strong>
-                                            <input type="checkbox" name="entrega" id='entrega' <?= $licitacao['entrega'] ? 'checked' : NULL ?> />
+                                            <input type="checkbox" class="check" name="entrega" id='entrega' <?= $licitacao['entrega'] ? 'checked' : NULL ?> />
                                         </label>                                    
                                     </div>                                  
                                 </div>
@@ -291,26 +291,72 @@ $status = recuperaDados("licitacao_status","id",$licitacao['licitacao_status_id'
 
 <script>
 
-    $('#num_processo').mask('0000.0000/0000000-0', {reverse: true});
+    // $('#num_processo').mask('0000.0000/0000000-0', {reverse: true});
 
-    function habilitarDesabilitarCampo(target, prop)
-    {
-        $(target).prop('disabled',prop);
+    // function habilitarDesabilitarCampo(target, prop)
+    // {
+    //     $(target).prop('disabled',prop);
+    // }
+
+    // function habilitaCampo(id) {
+    //     $('#' + id).prop('disabled');
+    //     if(document.getElementById(id).disabled==true){document.getElementById(id).disabled=false}
+    // }
+
+    // function desabilitarCampo(id){
+    //     if(document.getElementById(id).disabled==false){document.getElementById(id).disabled=true}
+    // }
+
+    // // 2 = habilitado
+    // // 1 = desaboçotado
+
+    // $(document).ready(function (){
+    //     alert('Pagina carregada');
+    // })
+</script>
+
+<script>
+
+let checks = document.querySelectorAll('.check');
+
+for (let i = 0; i <= 6; i++) {
+    if(checks[i].checked){
+        checks[i].disabled = false
+    }else{
+        let k = i; k++            
+        if(checks[k]){
+            checks[k].disabled = true
+        }
     }
 
-    function habilitaCampo(id) {
-        $('#' + id).prop('disabled');
-        if(document.getElementById(id).disabled==true){document.getElementById(id).disabled=false}
+}
+
+// Função que desabilita todos os checkbox que forem maiores que a checkbox === false
+const disableMaiorqueAtual = () => {
+    for (const key in checks) {
+        if(checks[key].disabled == true && checks[key].checked == true){
+            let cont = key
+            do {
+                checks[cont].checked = false
+                checks[cont].disabled = true
+                cont ++
+            }while(cont <= 6)
+        }
     }
+}
 
-    function desabilitarCampo(id){
-        if(document.getElementById(id).disabled==false){document.getElementById(id).disabled=true}
-    }
+for (let i = 0; i < checks.length; i++) {
+    const check = checks[i];
+  
+    check.addEventListener('change', () => {
+       
+        if(checks[i].checked === true){
+            checks[(i + 1 )].disabled = false
+        }else{
+            checks[(i + 1 )].disabled = true
+        }
+        disableMaiorqueAtual()
+    })  
+}
 
-    // 2 = habilitado
-    // 1 = desaboçotado
-
-    $(document).ready(function (){
-        alert('Pagina carregada');
-    })
 </script>
