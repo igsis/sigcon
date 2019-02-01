9c21cdb3ea5543de9441010776211917a099a313
Isaías Mendes <isaiaszmendes@gmail.com>
2019-02-01 16:00:04 -0200

    Alteração link do menu para pesquisar licitações e cadastrar contratos

--- perfil/contratos/includes/menu.php
+++ perfil/contratos/includes/menu.php
@@ -23,8 +23,8 @@
             <li class="treeview">
                 <a href=""> <i class="fa fa-circle-o text-red"></i> <span>Contratos</span> <i class="fa fa-angle-left pull-right"></i> </a>
                 <ul class="treeview-menu">
-                    <li><a href=""><i class="fa fa-circle-o text-red"></i> <span>Cadastrar</span></a></li>
-                    <li><a href="?perfil=contratos&p=pesquisa&sp=pesquisa_licitacao_contratos"><i class="fa fa-circle-o text-red"></i> <span>Pesquisar</span></a></li>
+                    <li><a href="?perfil=contratos&p=pesquisa&sp=pesquisa_licitacao_contratos"><i class="fa fa-circle-o text-red"></i> <span>Cadastrar</span></a></li>
+                    <li><a href=""><i class="fa fa-circle-o text-red"></i> <span>Pesquisar</span></a></li>
                 </ul>
             </li>
             <li class="treeview">
