c5eb22e639c4a800a985781d2dc61965c7408e57
Diego Vicente <diego.vdossantos@gmail.com>
2019-02-07 15:36:33 -0200

    ajuste modal de cancelamento de licitação

--- perfil/administrativo/pesquisa/pesquisa_licitacao.php
+++ perfil/administrativo/pesquisa/pesquisa_licitacao.php
@@ -103,16 +103,16 @@ $licitacoes = $conn->query("SELECT * FROM `licitacoes` WHERE publicado = '1'")->
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">×</span></button>
-                        <h4 class="modal-title">Cancelar Licitação</h4>
+                        <h4 class="modal-title">Confirma o Cancelamento da Licitação?</h4>
                     </div>
                     <div class="modal-body">
-                        <p>Tem certeza que deseja cancelar a licitação? <span> </span> </p>
+                        <p>Tem certeza que deseja cancelar a licitação <span> </span> </p>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Sair</button>
                         <form method='POST' id='formExcliuir'>
                             <input type="hidden" name='excluirLicitacao' value="<?= $licitacao['id'] ?>" >
-                            <button type='submit' class="btn btn-outline"> Cancelar </button>
+                            <button type='submit' class="btn btn-outline"> Sim </button>
                         </form>
                     </div>
                 </div>
