30e54523629a40ccbb4d7d0311d56b59d71681c8
Tanair <tanaiir@gmail.com>
2019-02-14 12:56:06 -0200

    Update equipamento_cadastro.php

--- perfil/administrativo/equipamentos/equipamento_cadastro.php
+++ perfil/administrativo/equipamentos/equipamento_cadastro.php
@@ -10,14 +10,14 @@ $con = bancoMysqli();
     <section class="content">
 
         <!-- START FORM-->
-        <h2 class="page-header">Edição de Equipamento</h2>
+        <h2 class="page-header">Cadastro de Equipamento</h2>
 
         <div class="row">
             <div class="col-md-12">
                 <!-- general form elements -->
                 <div class="box box-primary">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Unidades</h3>
+                        <h3 class="box-title">Equipamentos</h3>
                     </div>
                     <!-- /.box-header -->
                     <!-- form start -->
